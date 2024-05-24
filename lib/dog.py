8842969ec96b6de4027dfc4bdb7eb541a33c54f6
Tatiana Mathia <mathiatatiana@gmail.com>
2024-05-24 10:31:25 +0300

    attributes and properties

--- lib/dog.py
+++ lib/dog.py
@@ -12,4 +12,16 @@ APPROVED_BREEDS = [
 ]
 
 class Dog:
-    pass
+    def __init__(self, name=""):
+        self._name = name
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        if isinstance(value, str) and 1 <= len(value) <= 25:
+            self._name = value
+        else:
+            raise ValueError("Name must be a string between 1 and 25 characters.")
--- lib/person.py
+++ lib/person.py
@@ -16,4 +16,18 @@ APPROVED_JOBS = [
 ]
 
 class Person:
-    pass
+    def __init__(self, job=""):
+        self._job = ""
+        self.job = job
+
+    @property
+    def job(self):
+        return self._job
+
+    @job.setter
+    def job(self, value):
+        if value in self.APPROVED_JOBS:
+            self._job = value
+        else:
+            print("Job must be in list of approved jobs:", self.APPROVED_JOBS)
+        
